106d30ac92cd9d5355663109422f9bb309eb32cb
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2021-10-04 07:24:09 +0200

    mobile nav completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 console.log("working");
+const navMenu = document.querySelector(".header__hamburger-menu");
+const nav = document.querySelector(".header__nav");
+
+navMenu.addEventListener("click",()=>{
+    navMenu.classList.toggle("hamburger-menu--active");
+    nav.classList.toggle("header__nav--active")
+})
